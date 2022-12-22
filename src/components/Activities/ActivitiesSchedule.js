bacd6216d40ec96f05653c7ccfdfd2e82e2ede23
mmparadinha <matheusmparadinha@gmail.com>
2022-12-22 15:34:12 -0300

    fix: improved the day filter button to stay highlighted even when clicked outside

--- src/components/Activities/ActivitiesSchedule.js
+++ src/components/Activities/ActivitiesSchedule.js
@@ -13,7 +13,8 @@ import useToken from '../../hooks/useToken';
 export default function ActivitiesSchedule() {
   const token = useToken();
   const { activities } = useContext(ActivitiesContext);
-  const [activitiesDays, setActivitiesDays] = useState([]);
+  const [ activitiesDays, setActivitiesDays ] = useState([]);
+  const [ activeDay, setActiveDay ] = useState(null);
 
   async function listActivitiesDays() {
     try {
@@ -43,7 +44,7 @@ export default function ActivitiesSchedule() {
       <ActivitiesWrapper>
         {activities === null && <Message variant="h5">Primeiro, filtre pelo dia do evento:</Message>}
         <DaysBox>
-          {activitiesDays && activitiesDays.map((data, index) => <ActivitiesDay key={index} date={data} />)}
+          {activitiesDays && activitiesDays.map((data, index) => <ActivitiesDay key={index} date={data} activeDay={activeDay} setActiveDay={setActiveDay}/>)}
         </DaysBox>
         {activities !== null && <ActivitiesBox activities={activities} />}
       </ActivitiesWrapper>
